9a1a4ebda97972df2ab8595957889a64f2d599fe
Juve45 <juve45@rocketmail.com>
2017-03-20 13:27:32 +0200

    Added main stringservice and stringservicetest

---
+++ Kata1/src/kata1/StringService.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata1;
+
+/**
+ *
+ * @author alexandru
+ */
+public class StringService {
+    
+    public boolean isPalindrome(String string){
+        
+        StringBuilder str = new StringBuilder(string);
+        str.reverse();
+        String finalReversed = new String(str);
+        if(string.equals(str))
+            return true;
+        return false;
+    }
+}
